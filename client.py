bdcb56a387a526d948c0755c0b2e86163d20c79a
Vitor Yuske <vitor.yuski1@gmail.com>
2023-10-19 12:00:51 -0400

    Update on server.py and client.py

--- client.py
+++ client.py
@@ -8,41 +8,123 @@ class Client:
         self.buffer_size = buffer_size
         self.encoding = encoding
 
-        self.is_connected = False
-        self.name = ""
-        self.ip = ""
+        self.name = "CLIENT"
+        self.socket = self.connect_to_server()
+        self.ip = socket.gethostbyname(socket.gethostname())
         self.port = 1500
 
+        self.user_full_name = ""
+        self.user_nickname = ""
+        self.user_is_logged = False
+
     def connect_to_server(self):
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((self.server_ip, self.server_port))
+
+        return client_socket
+    
+    def login(self):
         full_name = input("Full name: ")
         nickname = input("Nickname: ")
         password = input("Password: ")
 
         message = {
-            "full_name": full_name,
-            "nickname" : nickname,
-            "password" : password
+            "message_type"  : "login",
+            "full_name"     : full_name,
+            "nickname"      : nickname,
+            "password"      : password
         }
 
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect((self.server_ip, self.server_port))
+        self.socket.send(pickle.dumps(message))
+        
+        response = self.socket.recv(self.buffer_size)
+        print ("[%s] Response: %s" %(self.name, response))
 
-        client_socket.send(pickle.dumps(message))
+    def create_profile(self):
+        full_name = input("Full name: ")
+        nickname = input("Nickname: ")
+        password = input("Password: ")
 
-        response = client_socket.recv(self.buffer_size)
+        message = {
+            "message_type"  : "create_profile",
+            "full_name"     : full_name,
+            "nickname"      : nickname,
+            "password"      : password
+        }
 
-        print ("Response: ", response)
+        self.socket.send(pickle.dumps(message))
 
-        client_socket.close()
+        response = self.socket.recv(self.buffer_size)
+        print ("[%s] Response: %s" %(self.name, response))
 
-SERVER_IP = "10.0.0.103"
+        return False
+    
+    def menu_login(self):
+        run_menu = True
+
+        while run_menu:
+            try:
+                option = int(input("[%s] Hello, select an option:\n0 - Login\n1 - Create profile\n2 - Exit\nAnswer: "%(self.name)))
+
+                match option:
+                    case 0: # Login
+                        run_menu = self.login()
+                        
+                    case 1: # Profile creation
+                        run_menu = self.create_profile()
+
+                    case 2: # Exit menu
+                        print("[%s] Good bye."%self.name)
+                        run_menu = False
+
+                    case _: # Invalid option
+                        print("[%s] Select an valid option."%self.name)
+
+            except ValueError as e: # Invalid option
+                print("[%s] Select an valid option."%self.name)
+
+    def menu_lobby(self):
+        run_menu = True
+
+        while run_menu:
+            try:
+                option = int(input("[%s] Select an option:\n0 - LIST-USER-ON-LINE\n1 - LIST-USER-PLAYING\n2 - Exit\nAnswer: "%(self.name)))
+
+                match option:
+                    case 0: # LIST-USER-ON-LINE
+                        print("[%s] LIST-USER-ON-LINE implementation"%self.name)
+                        run_menu = False
+                        
+                    case 1: # LIST-USER-PLAYING
+                        print("[%s] LIST-USER-PLAYING implementation"%self.name)
+                        run_menu = self.create_profile()
+
+                    case 2: # Exit menu
+                        print("[%s] Good bye."%self.name)
+                        run_menu = False
+
+                    case _: # Invalid option
+                        print("[%s] Select an valid option."%self.name)
+
+            except ValueError as e: # Invalid option
+                print("[%s] Select an valid option."%self.name)
+
+    def handle_connection(self):
+        self.menu_login()
+
+        if self.user_is_logged == True:
+            self.menu_lobby()
+
+        self.socket.close()
+
+SERVER_IP = "127.0.1.1"
 SERVER_PORT = 2001
 BUFFER_SIZE = 1024
 ENCODING = 'utf-8'
 
 def main():
     client = Client(server_ip=SERVER_IP, server_port=SERVER_PORT, buffer_size=BUFFER_SIZE, encoding=ENCODING)
-    client.connect_to_server()
+    client.handle_connection()
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -1,71 +1,94 @@
 import sys
 import socket
 import pickle
+import threading
 
 class Server:
-    def __init__(self, server_ip, server_port, buffer_size, encoding):
-        self.ip = server_ip
-        self.port = server_port
+    def __init__(self, name, ip, port, buffer_size, encoding):
+        self.name = name
+        self.ip = ip
+        self.port = port
         self.buffer_size = buffer_size
         self.socket = self.start_server()
         self.encoding = encoding
-
     
     def __str__(self):
         return f"{self.server_port}"
     
     def start_server(self):
-        print("Initializing server.")
-
+        print("[START] Initializing server.")
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         server_socket.bind((self.ip, self.port))
-        server_socket.listen(1)
-
         return server_socket
     
     def shutdown_server(self):
-        print("\nShutting down server.\n")
+        print("\n[STOP] Shutting down server.\n")
         self.socket.shutdown(socket.SHUT_RDWR)
         self.socket.close()
 
-    def start_listening_login(self):  
-        while True:
-            connection_socket, address = self.socket.accept()
-            
-            message = connection_socket.recv(self.buffer_size)
-            message = pickle.loads(message)
-
-            # Implementation of the login verification
-            output = self.login_verification(message)
-            
-            # Send response of the login status
-            connection_socket.send(output.encode(self.encoding))
-
-            print("Sent back to Client: %s" %(output))
-            
-            connection_socket.close()
     
-    def login_verification(self, message): # Raissa
+    def login_verification(self, message):
         full_name = message.get("full_name")
         nickname = message.get("nickname")
         password = message.get("password")
         
         print("Full_name: %s | Nickname: %s | Password: %s" %(full_name, nickname, password))
 
-        return full_name.upper()
+        return "[%s] %s"%(self.name, full_name.upper())
+    
+    def create_profile(self, message):
+
+        return "[%s] It's time to create your profile"%(self.name)
+    
+    def handle_menu_login(self, message, connection, address):
+        match message.get("message_type"):
+            case "login":
+                response = self.login_verification(message)
+
+            case "create_profile":
+                response = self.create_profile()
+
+            case _:
+                response = "[%s] Unknown type of message"
+
+        # Send response of the login status
+        connection.send(response.encode(self.encoding))
+
+        print("Sent back to Client: %s | Address: %s" %(response, address))
+
+    def handle_client(self, connection, address):
+        message = connection.recv(self.buffer_size)
+        message = pickle.loads(message)
+
+        self.handle_menu_login(message, connection, address)
+        
+        connection.close()
 
 
-SERVER_IP = "10.0.0.103"
+    def listen_to_client(self):
+        self.socket.listen(1)
+
+        while True:
+            connection, address = self.socket.accept()
+            thread = threading.Thread(target=self.handle_client, args=(connection, address))
+            thread.start()
+
+            print("[INFO] Active connections: %d" %(threading.active_count() - 1))
+
+
+SERVER_NAME = "SERVER"
+SERVER_IP = socket.gethostbyname(socket.gethostname())
 SERVER_PORT = 2001
 BUFFER_SIZE = 1024
 ENCODING = 'utf-8'
 
 def main():
-    server = Server(server_ip=SERVER_IP, server_port=SERVER_PORT, buffer_size=BUFFER_SIZE, encoding=ENCODING)
-    
+    server = Server(name=SERVER_NAME, ip=SERVER_IP, port=SERVER_PORT, buffer_size=BUFFER_SIZE, encoding=ENCODING)
+    print(SERVER_IP)
+
     try:
-        server.start_listening_login()
+        server.listen_to_client()
     except (KeyboardInterrupt):
         server.shutdown_server()
     
@@ -74,9 +97,7 @@ def main():
 if __name__ == "__main__":
     main()
 
-
-# IMPLEMENTAR
-#   Login
-#
-#
+# Implementar
+#   login
+#   monitoramento de usuários
 #
