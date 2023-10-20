426bc2c1abe1dd124049023131b48d25440dd57d
Vitor Yuske <vitor.yuski1@gmail.com>
2023-10-20 16:49:30 -0400

    Update on server.py and client.py. Added the login menu implementation.

--- client.py
+++ client.py
@@ -1,61 +1,129 @@
+import sys
 import socket
 import pickle
 
 class Client:
-    def __init__(self, server_ip, server_port, buffer_size, encoding):
+    def __init__(self, server_ip, server_port, buffer_size, encoding, name, ip, port, menu_string):
         self.server_ip = server_ip
         self.server_port = server_port
         self.buffer_size = buffer_size
         self.encoding = encoding
 
-        self.name = "CLIENT"
+        self.name = name
         self.socket = self.connect_to_server()
-        self.ip = socket.gethostbyname(socket.gethostname())
-        self.port = 1500
+        self.ip = ip
+        self.port = port
+        self.menu_string = menu_string
 
         self.user_full_name = ""
         self.user_nickname = ""
-        self.user_is_logged = False
+        self.user_logged = False
+
+    def set_user_logged(self, user_logged):
+        self.user_logged = user_logged
 
     def connect_to_server(self):
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect((self.server_ip, self.server_port))
+        try:
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket.connect((self.server_ip, self.server_port))
 
-        return client_socket
-    
-    def login(self):
-        full_name = input("Full name: ")
-        nickname = input("Nickname: ")
-        password = input("Password: ")
-
-        message = {
-            "message_type"  : "login",
-            "full_name"     : full_name,
-            "nickname"      : nickname,
-            "password"      : password
-        }
-
-        self.socket.send(pickle.dumps(message))
+            return client_socket
         
-        response = self.socket.recv(self.buffer_size)
-        print ("[%s] Response: %s" %(self.name, response))
-
-    def create_profile(self):
-        full_name = input("Full name: ")
-        nickname = input("Nickname: ")
-        password = input("Password: ")
-
+        except ConnectionRefusedError as e:
+            print("[%s] ERROR: Unable to connect to server."%self.name)
+    
+    def disconnect_to_server(self):
         message = {
-            "message_type"  : "create_profile",
-            "full_name"     : full_name,
-            "nickname"      : nickname,
-            "password"      : password
+            "type": "exit_server"
         }
-
         self.socket.send(pickle.dumps(message))
+        self.socket.close()
 
-        response = self.socket.recv(self.buffer_size)
-        print ("[%s] Response: %s" %(self.name, response))
+    def login(self):
+        run_login = True
+        while run_login:
+            nickname = input("Nickname: ")
+            password = input("Password: ")
+
+            message = {
+                "type"      : "login",
+                "nickname"  : nickname,
+                "password"  : password
+            }
+
+            self.socket.send(pickle.dumps(message))
+            response = pickle.loads(self.socket.recv(self.buffer_size))
+            self.set_user_logged(response.get("logged"))
+
+            if response.get("logged"):
+                print("[%s] You logged in the server." %(self.name))
+                return False
+            
+            else:
+                run_menu = True
+                while run_menu:
+                    try:
+                        option = int(input("[%s] Unable to log into account. Select an option:\n0 - Try again\n1 - Exit\nAnswer: " %(self.name)))
+                    
+                        match option:
+                            case 0: # Try again
+                                run_login = True
+                                run_menu = False
+
+                            case 1: # Exit menu
+                                return True
+
+                            case _: # Invalid option
+                                print("[%s] Select an valid option."%self.name)
+                
+                    except ValueError as e: # Invalid option
+                        print("[%s] Select an valid option."%self.name)
+                    
+                    print(self.menu_string)
+
+    def create_account(self):
+        run_account_creation = True
+        while run_account_creation:
+            full_name = input("Full name: ")
+            nickname = input("Nickname: ")
+            password = input("Password: ")
+
+            message = {
+                "type"       : "create_account",
+                "full_name"  : full_name,
+                "nickname"   : nickname,
+                "password"   : password
+            }
+
+            self.socket.send(pickle.dumps(message))
+            response = pickle.loads(self.socket.recv(self.buffer_size))
+            self.set_user_logged(response.get("logged"))
+
+            if response.get("logged"):
+                print("[%s] You logged into the server." %(self.name))
+                return False
+            
+            else:
+                run_menu = True
+                while run_menu:
+                    try:
+                        option = int(input("[%s] Unable to create account. Select an option:\n0 - Try again\n1 - Exit\nAnswer: " %(self.name)))
+                    
+                        match option:
+                            case 0: # Try again
+                                run_account_creation = True
+                                run_menu = False
+
+                            case 1: # Exit menu
+                                return True
+
+                            case _: # Invalid option
+                                print("[%s] Select an valid option."%self.name)
+                
+                    except ValueError as e: # Invalid option
+                        print("[%s] Select an valid option."%self.name)
+                    
+                    print(self.menu_string)
 
         return False
     
@@ -64,14 +132,14 @@ class Client:
 
         while run_menu:
             try:
-                option = int(input("[%s] Hello, select an option:\n0 - Login\n1 - Create profile\n2 - Exit\nAnswer: "%(self.name)))
+                option = int(input("[%s] Select an option:\n0 - Login\n1 - Create profile\n2 - Exit\nAnswer: "%(self.name)))
 
                 match option:
                     case 0: # Login
                         run_menu = self.login()
                         
-                    case 1: # Profile creation
-                        run_menu = self.create_profile()
+                    case 1: # Account creation
+                        run_menu = self.create_account()
 
                     case 2: # Exit menu
                         print("[%s] Good bye."%self.name)
@@ -79,9 +147,17 @@ class Client:
 
                     case _: # Invalid option
                         print("[%s] Select an valid option."%self.name)
-
+        
             except ValueError as e: # Invalid option
                 print("[%s] Select an valid option."%self.name)
+
+            print(self.menu_string)
+        
+        message = {
+            "type": "exit_menu_login"
+        }
+
+        self.socket.send(pickle.dumps(message))
 
     def menu_lobby(self):
         run_menu = True
@@ -93,11 +169,11 @@ class Client:
                 match option:
                     case 0: # LIST-USER-ON-LINE
                         print("[%s] LIST-USER-ON-LINE implementation"%self.name)
-                        run_menu = False
+                        run_menu = True
                         
                     case 1: # LIST-USER-PLAYING
                         print("[%s] LIST-USER-PLAYING implementation"%self.name)
-                        run_menu = self.create_profile()
+                        run_menu = True
 
                     case 2: # Exit menu
                         print("[%s] Good bye."%self.name)
@@ -108,23 +184,54 @@ class Client:
 
             except ValueError as e: # Invalid option
                 print("[%s] Select an valid option."%self.name)
+            
+            print(self.menu_string)
 
     def handle_connection(self):
-        self.menu_login()
+        try:
+            if self.socket == None:
+                return 0
 
-        if self.user_is_logged == True:
-            self.menu_lobby()
+            self.menu_login()
 
-        self.socket.close()
+            if self.user_logged == True:
+                self.menu_lobby()
 
-SERVER_IP = "127.0.1.1"
+            self.disconnect_to_server()
+
+        except EOFError as e:
+            print("[%s] ERROR: Connection to the server was lost. Try again later."%self.name)
+            self.socket.close()
+        except KeyboardInterrupt as e:
+            self.disconnect_to_server()
+
+
+SERVER_IP = "10.0.0.102"
 SERVER_PORT = 2001
 BUFFER_SIZE = 1024
 ENCODING = 'utf-8'
 
+CLIENT_NAME = "CLIENT"
+CLIENT_IP = "10.0.0.102"
+CLIENT_PORT = 1500
+
+MENU_STRING = "--------------------"
+
 def main():
-    client = Client(server_ip=SERVER_IP, server_port=SERVER_PORT, buffer_size=BUFFER_SIZE, encoding=ENCODING)
+    client = Client(
+        server_ip=SERVER_IP,
+        server_port=SERVER_PORT,
+        buffer_size=BUFFER_SIZE,
+        encoding=ENCODING,
+        name=CLIENT_NAME,
+        ip=CLIENT_IP,
+        port=CLIENT_PORT,
+        menu_string=MENU_STRING
+    )
+
     client.handle_connection()
+
 
 if __name__ == "__main__":
     main()
+    sys.exit(0)
--- server.py
+++ server.py
@@ -16,88 +16,119 @@ class Server:
         return f"{self.server_port}"
     
     def start_server(self):
-        print("[START] Initializing server.")
+        print("[%s] Initializing server."%self.name)
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         server_socket.bind((self.ip, self.port))
         return server_socket
     
     def shutdown_server(self):
-        print("\n[STOP] Shutting down server.\n")
+        print("[%s] Shutting down server."%self.name)
         self.socket.shutdown(socket.SHUT_RDWR)
         self.socket.close()
-
     
     def login_verification(self, message):
+        # Implementation the process of verification of the login account and make return True or False if the user is online        
+        nickname = message.get("nickname")
+        password = message.get("password")
+
+        response = {
+            "type": "login"
+        }
+        if nickname == "wrong_nickname" or password == "wrong_password":
+            response.update({"logged": False})
+        else:
+            response.update({"logged": True})
+
+        return response
+    
+    def create_account_verification(self, message):
+        # Implementation of the process of profile creation and login on new account if possible
         full_name = message.get("full_name")
         nickname = message.get("nickname")
         password = message.get("password")
+
+        response = {
+            "type": "create_account"
+        }
+        if full_name == "wrong_full_name" or nickname == "wrong_nickname" or password == "wrong_password":
+            response.update({"logged": False})
+        else:
+            response.update({"logged": True})
+
+        return response
+    
+    def handle_menu_login(self, connection, address):
+        message = pickle.loads(connection.recv(self.buffer_size))
+        print("[%s] LOGIN\nMessage: %s\nAddress: %s" %(self.name, message, address))
+
+        while message.get("type") != "exit_menu_login" and message.get("type") != "exit_server":
+            match message.get("type"):
+                case "login":
+                    response = self.login_verification(message)
+
+                case "create_account":
+                    response = self.create_account_verification(message)
+
+                case _:
+                    response = "[%s] Unknown type of message."%self.name
+
+            print("[%s] LOGIN\nResponse: %s\nAddress: %s"%(self.name, response, address))
+
+            connection.send(pickle.dumps(response))
+            message = pickle.loads(connection.recv(self.buffer_size))
+            print("[%s] LOGIN\nMessage: %s\nAddress: %s" %(self.name, message, address))
         
-        print("Full_name: %s | Nickname: %s | Password: %s" %(full_name, nickname, password))
+        return message
 
-        return "[%s] %s"%(self.name, full_name.upper())
-    
-    def create_profile(self, message):
-
-        return "[%s] It's time to create your profile"%(self.name)
-    
-    def handle_menu_login(self, message, connection, address):
-        match message.get("message_type"):
-            case "login":
-                response = self.login_verification(message)
-
-            case "create_profile":
-                response = self.create_profile()
-
-            case _:
-                response = "[%s] Unknown type of message"
-
-        # Send response of the login status
-        connection.send(response.encode(self.encoding))
-
-        print("Sent back to Client: %s | Address: %s" %(response, address))
 
     def handle_client(self, connection, address):
-        message = connection.recv(self.buffer_size)
-        message = pickle.loads(message)
+        try:
+            message = self.handle_menu_login(connection, address)
+            print("[%s] Connection to %s is closed."%(self.name, address))
+            connection.close()
 
-        self.handle_menu_login(message, connection, address)
-        
-        connection.close()
-
+        except (EOFError, ConnectionResetError) as e:
+            print("[%s] ERROR %s lost connection."%(self.name, address))
+            connection.close()
 
     def listen_to_client(self):
         self.socket.listen(1)
 
-        while True:
-            connection, address = self.socket.accept()
-            thread = threading.Thread(target=self.handle_client, args=(connection, address))
-            thread.start()
+        try:
+            while True:
+                connection, address = self.socket.accept()
+                thread = threading.Thread(target=self.handle_client, args=(connection, address))
+                thread.daemon = True
+                thread.start()
 
-            print("[INFO] Active connections: %d" %(threading.active_count() - 1))
-
+                print("[%s] Active connections: %d" %(self.name, threading.active_count() - 1))
+        except (KeyboardInterrupt) as e:
+            self.shutdown_server()
 
 SERVER_NAME = "SERVER"
-SERVER_IP = socket.gethostbyname(socket.gethostname())
+SERVER_IP = "10.0.0.102"
 SERVER_PORT = 2001
 BUFFER_SIZE = 1024
 ENCODING = 'utf-8'
 
 def main():
-    server = Server(name=SERVER_NAME, ip=SERVER_IP, port=SERVER_PORT, buffer_size=BUFFER_SIZE, encoding=ENCODING)
-    print(SERVER_IP)
+    server = Server(
+        name=SERVER_NAME,
+        ip=SERVER_IP,
+        port=SERVER_PORT,
+        buffer_size=BUFFER_SIZE,
+        encoding=ENCODING
+    )
 
-    try:
-        server.listen_to_client()
-    except (KeyboardInterrupt):
-        server.shutdown_server()
-    
-    sys.exit(0)
+    server.listen_to_client()
 
 if __name__ == "__main__":
     main()
+    sys.exit(0)
 
 # Implementar
 #   login
+#       
 #   monitoramento de usuários
 #
